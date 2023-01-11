49b834555c52b3bc9e6809626028189d6be44389
Carlos Medina <cmedi016@gmail.com>
2023-01-11 00:35:55 +0000

    modified changeImage.py to convert tiffs to jpegs in the right size

--- changeImage.py
+++ changeImage.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
 from PIL import Image
-
+import os
+import os.path
 #directory
 #supplier/data/images/
+
+dir = 'supplier-data/images/'
+size = (600,400)
+
+for file in os.listdir(dir):
+  if file.endswith(".tiff"):
+    im=Image.open(os.path.join(dir,file))
+    im = im.convert("RGB")
+    im = im.resize(size)
+    im.save(os.path.join(dir,file.replace('.tiff','.jpeg')),'JPEG')
+    print(im.format, im.size, im.mode)
+    
+
 
 
