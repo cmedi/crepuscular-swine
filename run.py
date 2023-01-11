cf021bc9869c60c48eb2c08c8da6ea54eda38b4c
Carlos Medina <cmedi016@gmail.com>
2023-01-11 01:20:42 +0000

    nearly finished run.py, only making this commit because the vm access time is running out

--- run.py
+++ run.py
@@ -2,14 +2,17 @@
 
 import requests
 import os
+import os.path
 
-url = http://linux-instance-external-IP/fruits
+url = r'http://linux-instance-external-IP/fruits'
 dir = 'supplier-data/descriptions/'
 
 fruits = []
 fruit = {'name':'', 'weight':'', 'description':0, 'image_name':''}
 
+files = os.listdir(dir)
 
-
-
-
+for description in files:
+ path = os.path.join(dir,description)
+ with open(path, 'r') as opened:
+   print(len(opened.readlines()))
