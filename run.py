5148922d17b6341f58a54e893dcfe95a2f26cc85
Carlos Medina <cmedi016@gmail.com>
2023-01-11 01:10:11 +0000

    run.py begins

---
+++ run.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import requests
+import os
+
+url = http://linux-instance-external-IP/fruits
+dir = 'supplier-data/descriptions/'
+
+fruits = []
+fruit = {'name':'', 'weight':'', 'description':0, 'image_name':''}
+
+
+
+
+
