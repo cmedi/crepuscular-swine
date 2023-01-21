bed0ff7a519c4517500b9d671afc42b4b4114806
carlos medina <118164798+cmedi@users.noreply.github.com>
2023-01-20 18:54:01 -0700

    Update collatz.py
    handled possible valueError exceptions. or just general don't-follow-instructionness.

--- scripts/collatz.py
+++ scripts/collatz.py
@@ -2,23 +2,29 @@ import sys
 
 def collatz(number):
     
-    if number == 1:
-        print('This is the end.')
-        return number
-        sys.exit()
+        if number == 1:
+            print('This is the end.')
+            return number
+            sys.exit()
     
-    # floor divide even number
-    if number % 2 == 0:
-        number = number // 2
-        print (number)
+        #even
+        if number % 2 == 0:
+            number = number // 2
+            print (number)
         
-    # transform odd number    
-    elif number % 2 == 1:
-        number = 3 * number + 1
-        print(number)
+        #odd    
+        elif number % 2 == 1:
+            number = 3 * number + 1
+            print(number)
     
-    return collatz(number)
+        return collatz(number)
+    
         
 
 if __name__ == "__main__":
-    one_at_the_end = collatz(int (input() ) )
+    try:
+        user_input = int (input ('Give us an integer. \n') )
+        collatz( user_input )
+     
+    except Exception:
+        print('i really meant it about the integer \n ')
